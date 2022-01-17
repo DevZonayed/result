d9191c892777f736dbf14e8b2f26aef1bfa76524
devzonayed <zonayedofficial@gmail.com>
2022-01-18 01:55:22 +0600

    complete

--- function.js
+++ function.js
@@ -16,3 +16,25 @@ function s1(value){
 }
 
 // ------------------------
+
+//result details function start
+function grade(mark){
+    if(mark < 0 || mark > 100){
+        return 'Invalid !';
+    }else if(mark < 32){
+        return 'F';
+    }else if(mark >= 33 && mark <= 39){
+        return 'D'
+    }else if(mark >= 40 && mark <= 49){
+        return 'C'
+    }else if(mark >= 50 && mark <= 59){
+        return 'B'
+    }else if(mark >= 60 && mark <= 69){
+        return 'A-'
+    }else if(mark >= 70 && mark <= 79){
+        return 'A'
+    }else if(mark >= 80 && mark <= 100){
+        return 'A+'
+    }
+}
+//result details function end
